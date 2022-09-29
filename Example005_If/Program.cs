5f8d92f889fb92ae2403310e3866cce44dacbeec
Dmitriy <1111born1111@gmail.com>
2022-09-30 01:00:34 +0300

    If else

---
+++ Example005_If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Отдельный привет для Маши");
+}
+else
+{
+    Console.Write("Имя пользователя: " + username);
+}
